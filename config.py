ca328d8bf29643525ad83d22136ebae99fefbaf8
styled <smyvensestime@gmail.com>
2021-12-23 19:11:31 -0500

    Initial Commit

--- config.py
+++ config.py
@@ -3,31 +3,31 @@ import configparser
 
 class Config:
     def __init__(self, **kwargs):
-        self.file_dir = 'config.ini'
-        self.config_file = configparser.ConfigParser()
-        self.config_file.read(self.file_dir)
+        self.__file_dir = 'config.ini'
+        self.__get_data = lambda section, key: self.__read_file()[section][key]
 
-        self.window_width = int(self.config_file['Setup']['Width'])
-        self.window_height = int(self.config_file['Setup']['Height'])
+        self.window_width = int(self.__get_data('Setup', 'Width'))
+        self.window_height = int(self.__get_data('Setup', 'Height'))
 
-        self.fps = float(self.config_file['Gameplay']['Fps'])
-        self.snake_speed = int(self.config_file['Gameplay']['InitialSnakeSpeed'])
-        self.high_score = int(self.config_file['Gameplay']['HighScore'])
+        self.fps = float(self.__get_data('Gameplay', 'Fps'))
+        self.snake_speed = int(self.__get_data('Gameplay', 'snake_speed'))
 
-        self.fruit_size = int(self.config_file['Settings']['FruitSize'])
-        self.snake_size = int(self.config_file['Settings']['SnakeSize'])
+        self.fruit_size = int(self.__get_data('Settings', 'fruit_size'))
+        self.snake_size = int(self.__get_data('Settings', 'snake_size'))
 
-        self.move_up = self.config_file['Controls']['MoveUp'].lower()
-        self.move_down = self.config_file['Controls']['MoveDown'].lower()
-        self.move_left = self.config_file['Controls']['MoveLeft'].lower()
-        self.move_right = self.config_file['Controls']['MoveRight'].lower()
-        self.restart_game = self.config_file['Controls']['Restart'].lower()
+        self.move_up = self.__get_data('Controls', 'move_up').lower()
+        self.move_down = self.__get_data('Controls', 'move_down').lower()
+        self.move_left = self.__get_data('Controls', 'move_left').lower()
+        self.move_right = self.__get_data('Controls', 'move_right').lower()
+        self.restart_game = self.__get_data('Controls', 'Restart').lower()
 
-    def write_config_file(self, section, key, value, new_section=False):
-        if new_section:
-            self.config_file.add_section(section)
+    def __read_file(self):
+        config_file = configparser.ConfigParser()
+        config_file.read(self.__file_dir)
+        return config_file
 
-        self.config_file.set(section, key, value)
-
-        with open(self.file_dir) as file:
-            self.config_file.write(file)
+    def write_config_file(self, section, key, value, ):
+        file = self.__read_file()
+        file.set(section, key, value)
+        with open(self.__file_dir, 'w') as f:
+            file.write(f)
--- main.py
+++ main.py
@@ -12,31 +12,24 @@ from config import Config
 class SnakeFruit(Widget, Config):
     def __init__(self, **kwargs):
         super(SnakeFruit, self).__init__(**kwargs)
-
         # set some default values
         self.size = (self.fruit_size, self.fruit_size)
         self.new_pos()
 
-    def is_collision(self, snake):
-        if self.collide_widget(snake):
-            return True
-
     # send the fruit to a random position within the screen
-    def new_pos(self, exclude_cords=None):
-        def random_x_cord():
-            return self.width * round(random.randint(0, self.window_width - self.width) / self.width)
-
-        def random_y_cord():
-            return self.width * round(random.randint(0, self.window_height - self.width) / self.width)
+    def new_pos(self, exclude_cords: list = None):
+        # random number that is divisible by the width of the fruit, to make sure the fruit is always on a grid
+        def random_num(start, end):
+            return self.width * round(random.randint(start, end - self.width) / self.width)
 
         # get a new random position for the fruit
-        cords = (random_x_cord(), random_y_cord())
+        cords = [random_num(0, self.window_width), random_num(0, self.window_height)]
 
         # exclude_cords: where the snake is located
         # if the new position of the fruit is where the snake is located, get a new position
         if exclude_cords:
             for i in range(len(exclude_cords)):
-                if exclude_cords[i].x == cords[0] and exclude_cords[i].x == cords[1]:
+                if cords == exclude_cords[i]:
                     self.new_pos(exclude_cords)
 
         self.pos = cords
@@ -62,28 +55,36 @@ class SnakeSegment(Widget, Config):
         self.y += y
 
 
+class Grid(Widget, Config):
+    def __init__(self, **kwargs):
+        super(Grid, self).__init__(**kwargs)
+        self.size = (self.snake_size, self.snake_size)
+        self.pos = (0, 0)
+
+
 # overall logic for the game
 class SnakeGame(Widget, Config):
     score = NumericProperty(0)
-    message = StringProperty()
 
     def __init__(self, **kwargs):
         super(SnakeGame, self).__init__(**kwargs)
-        # for keyboard events
-        Window.bind(on_key_down=self.keyboard_event)
         # set the window to the correct size
         Window.size = (self.window_width, self.window_height)
         # run the update function with an interval based on fps value
         Clock.schedule_interval(self.update, 1 / self.fps)
+
+        # create a grid in the background of the window
+        for i in range(self.window_width // self.snake.width):
+            for j in range(self.window_height // self.snake.height):
+                background_grid = Grid()
+                background_grid.pos = (i * self.snake.width, j * self.snake.height)
+                self.add_widget(background_grid)
 
         # set some default values for the snake
         self.original_speed = self.snake_speed
         self.direction = [0, 0]
         self.snake_segments = []
         self.is_running = True if (self.direction[0] == self.direction[0]) else False
-
-        # add a segment to the snake
-        # self.add_snake_segment()
 
     def add_snake_segment(self):
         new_segment = SnakeSegment()
@@ -106,17 +107,23 @@ class SnakeGame(Widget, Config):
         self.direction = [0, 0]
         self.is_running = True
 
+    def pause(self):
+        self.direction = [0, 0]
+
     # miscellaneous functions that are very important
     def update(self, *args):
-        if self.is_running:
-            # remove any messages from the screen
-            self.message = ''
+        assert args
 
+        # start listening for keyboard events on this frame
+        Window.bind(on_key_down=self.keyboard_event)
+
+        if self.is_running:
             # check for collision between snake and fruit
             if self.fruit.x == self.snake.x and self.fruit.y == self.snake.y:
                 self.score += 1
                 self.fruit.new_pos(self.snake_segments)
                 self.add_snake_segment()
+                self.fps += 10
 
             # check for collision between snake and the right side of the screen
             if self.snake.x + self.direction[0] >= self.width:
@@ -143,32 +150,34 @@ class SnakeGame(Widget, Config):
             if self.snake_segments:
                 # make all segments follow each other starting from the back
                 for i in range(len(self.snake_segments) - 1, 0, -1):
-                    self.snake_segments[i].pos = (self.snake_segments[i - 1].x, self.snake_segments[i - 1].y)
+                    self.snake_segments[i].pos = self.snake_segments[i - 1].pos
                 # the second segment will have the same coordinates as the head
                 self.snake_segments[0].pos = (self.snake.x, self.snake.y)
 
             # constantly move the snake
             self.snake.move(self.direction[0], self.direction[1])
 
-        else:
-            self.message = '               Game Over \n' \
-                           f'Press the \"{self.restart_game.upper()}\" key to restart!'
-            self.message_label.center_x += 0
-
-    # keyboard events
     def keyboard_event(self, *args):
         key_code = list(args)[3]
         # listen for controls from user while the game is running
         if self.is_running:
+            """
             # if the user press the key to move towards a direction,
-            # and they are not currently moving in the opposite direction
+             and they are not currently moving in the opposite direction
+                # stop listening for new keyboard events
+                # give instructions in the new direction chosen
+            """
             if key_code == self.move_up and self.direction[1] == 0:
+                Window.unbind(on_key_down=self.keyboard_event)
                 self.direction = [0, self.snake_speed]
             elif key_code == self.move_down and self.direction[1] == 0:
+                Window.unbind(on_key_down=self.keyboard_event)
                 self.direction = [0, (self.snake_speed * -1)]
             elif key_code == self.move_right and self.direction[0] == 0:
+                Window.unbind(on_key_down=self.keyboard_event)
                 self.direction = [self.snake_speed, 0]
             elif key_code == self.move_left and self.direction[0] == 0:
+                Window.unbind(on_key_down=self.keyboard_event)
                 self.direction = [(self.snake_speed * -1), 0]
 
         if not self.is_running:
@@ -184,9 +193,3 @@ class SnakeApp(App):
 
 if __name__ == '__main__':
     SnakeApp().run()
-
-
-# TODO: save high score
-# TODO: Make game over label above snake and fruit
-# TODO: Make snake go faster as time goes by
-# FIXME: death if you change direction too fast
